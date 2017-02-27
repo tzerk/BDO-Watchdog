3fa28162c638df6ca7371a71ba48af12351dd5cb
tzerk <christoph.burow@gmx.net>
2017-02-27 17:51:08 +0100

    fix critical bug in progress bar
    value in pb must not be greater than 100

--- main.go
+++ main.go
@@ -275,11 +275,17 @@ func observer(
 // A wrapper for time.Sleep() that also updates the UI label and progressbar
 func wait(config Config, label_Update *ui.Label, pb *ui.ProgressBar) {
 	tstep := config.TimeBetweenChecksInS
+	var pbVal int
+
 	if tstep <= 0 {
 		tstep = 1
 	} // otherwise division by 0
 	for i := 0; i <= tstep; i++ {
-		pb.SetValue(int(100/float32(tstep) * float32(i + 1)))
+		pbVal = int(100/float32(tstep) * float32(i))
+		if pbVal > 100 {
+			pbVal = 100
+		}
+		pb.SetValue(pbVal)
 		label_Update.SetText("  Next update in... " + strconv.Itoa(tstep - i) + " s")
 		time.Sleep(1 * time.Second)
 	}
